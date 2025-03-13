38820f8f931dcc9662ce5366d018cb6b8a3b654b
Israel Akinoso <akinosoissy@gmail.com>
2025-03-13 13:48:46 +0000

    Complete Phrases api filter options

--- server/routes/phraseRoutes.js
+++ server/routes/phraseRoutes.js
@@ -2,20 +2,67 @@ import express from "express";
 import connection from "../utils/mysql.js";
 const router = express.Router();
 
-router.get("/", async (_req, res) => {
-  const sql = `SELECT * FROM phrases`;
+router.get("/", async (req, res) => {
+  const { languageId, settingsId } = req.query;
 
-  try {
-    const [results] = await connection.query(sql);
+  // If both Query params are provided
+  if (languageId && settingsId) {
+    try {
+      const sql = `SELECT * from phrases 
+                   WHERE language_id = ? and setting_id = ?
+      `;
+      const [results] = await connection.query(sql, [languageId, settingsId]);
+
+      if (!results.length) {
+        res.status(404).json({ msg: "No phrases to match your query" });
+        return;
+      }
+
+      res.json(results);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  } else if (languageId) {
+    // If only Language Id is provided
+    try {
+      const sql = `SELECT * from phrases WHERE language_id = ? `;
+      const [results] = await connection.query(sql, [languageId]);
+
+      if (!results.length) {
+        res.status(404).json({ msg: "No phrases to match your query" });
+        return;
+      }
+
+      res.json(results);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  } else if (settingsId) {
+    // if only settings Id is provided
+    const sql = `SELECT * from phrases WHERE setting_id = ? `;
+    const [results] = await connection.query(sql, [settingsId]);
 
     if (!results.length) {
-      res.status(404).json({ msg: "No phrases in DB" });
+      res.status(404).json({ msg: "No phrases to match your query" });
       return;
     }
 
     res.json(results);
-  } catch (error) {
-    res.status(500).json(error);
+  } else {
+    // If just the default route is provided
+    try {
+      const sql = `SELECT * FROM phrases`;
+      const [results] = await connection.query(sql);
+
+      if (!results.length) {
+        res.status(404).json({ msg: "No phrases in DB" });
+        return;
+      }
+
+      res.json(results);
+    } catch (error) {
+      res.status(500).json(error);
+    }
   }
 });
 
